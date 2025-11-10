fd5750b29d22eda3e160f2e09006d8a9c05254ca
Ricky Chen <rc3297@drexel.edu>
2025-11-10 16:23:43 -0500

    hw3: finished close connection

--- hw3-http/client-cc.c
+++ hw3-http/client-cc.c
@@ -5,34 +5,36 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 #define  BUFF_SZ 1024
 
 char recv_buff[BUFF_SZ];
 
 char *generate_cc_request(const char *host, int port, const char *path){
-	static char req[512] = {0};
-	int offset = 0;
-	
-    //note that all paths should start with "/" when passed in
-	offset += sprintf((char *)(req + offset),"GET %s HTTP/1.1\r\n", path);
-	offset += sprintf((char *)(req + offset),"Host: %s\r\n", host);
-	offset += sprintf((char *)(req + offset),"Connection: Close\r\n");
-	offset += sprintf((char *)(req + offset),"\r\n");
+    static char req[512] = {0};
+    int offset = 0;
 
-	printf("DEBUG: %s", req);
-	return req;
+    //note that all paths should start with "/" when passed in
+    offset += sprintf((char *)(req + offset),"GET %s HTTP/1.1\r\n", path);
+    offset += sprintf((char *)(req + offset),"Host: %s\r\n", host);
+    offset += sprintf((char *)(req + offset),"Connection: Close\r\n");
+    offset += sprintf((char *)(req + offset),"\r\n");
+
+    printf("DEBUG: %s", req);
+    return req;
 }
 
 
 void print_usage(char *exe_name){
     fprintf(stderr, "Usage: %s <hostname> <port> <path...>\n", exe_name);
-    fprintf(stderr, "Using default host %s, port %d  and path [\\]\n", DEFAULT_HOST, DEFAULT_PORT); 
+    fprintf(stderr, "Using default host %s, port %d  and path [\\]\n", DEFAULT_HOST, DEFAULT_PORT);
 }
 
 int process_request(const char *host, uint16_t port, char *resource){
     int sock;
-    int total_bytes;
+    int total_bytes = 0;
+    int bytes_recvd = 0;
 
     sock = socket_connect(host, port);
     if(sock < 0) return sock;
@@ -47,21 +49,38 @@ int process_request(const char *host, uint16_t port, char *resource){
     //    loop, and you must save the data received inside of recv_buff.
     // 4. Each interation through the loop print out the data you receive.
     //    Note, the data will not be null terminated so be careful that
-    //    you use the size of the data returned to control how the data 
+    //    you use the size of the data returned to control how the data
     //    is printed.  Here is a format string that can help you out.
-    //  
+    //
     //        printf("%.*s", bytes_recvd, recv_buff);
     //
     // 5. This function should return the total number of bytes received
     //    from the server, so why you are looping around, make sure to
-    //    accumulate all of the data received and return this value. 
+    //    accumulate all of the data received and return this value.
     //---------------------------------------------------------------------------------
+    const char* request = generate_cc_request(host, port, resource);
+    int request_len = strlen(request);
+
+    int sent_size = send(sock, request, request_len, 0);
+    if(sent_size != request_len) {
+        perror("send failed");
+        close(sock);
+        return -1;
+    }
+
+    while((bytes_recvd = recv(sock, recv_buff, BUFF_SZ, 0)) > 0) {
+        printf("%.*s", bytes_recvd, recv_buff);
+        total_bytes += bytes_recvd;
+    }
 
     close(sock);
     return total_bytes;
 }
 
 int main(int argc, char *argv[]){
+    clock_t start, end;
+    start = clock();
+
     int sock;
 
     const char *host = DEFAULT_HOST;
@@ -75,7 +94,7 @@ int main(int argc, char *argv[]){
         print_usage(argv[0]);
         //process the default request
         process_request(host, port, resource);
-	} else {
+    } else {
         host = argv[1];
         port = atoi(argv[2]);
         resource = argv[3];
@@ -91,4 +110,9 @@ int main(int argc, char *argv[]){
             process_request(host, port, resource);
         }
     }
+
+
+    end = clock();
+    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("\n\nProgram execution time: %.6f seconds\n", cpu_time_used);
 }
